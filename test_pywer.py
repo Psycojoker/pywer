33212ffd8d04591d4f9e48ecf162657f846a6657
Laurent Peuch <cortex@worlddomination.be>
2014-12-16 02:38:09 +0100

    [enh] more tests on package_to_uri

--- test_pywer.py
+++ test_pywer.py
@@ -1,5 +1,13 @@
 from pywer import package_to_uri
 
 
-def test_package_to_uri():
+def test_package_to_uri_git_proto():
     assert package_to_uri("git://github.com/user/package.git") == "git://github.com/user/package.git"
+
+
+def test_package_to_uri_https():
+    assert package_to_uri("https://example.com/script.js") == "https://example.com/script.js"
+
+
+def test_package_to_uri_http():
+    assert package_to_uri("http://example.com/script.js") == "http://example.com/script.js"
